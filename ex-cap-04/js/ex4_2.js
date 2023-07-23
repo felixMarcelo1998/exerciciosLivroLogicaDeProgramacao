ae96d015798abcb0a4fea6175ef527d94a38864b
felixmarcelo7 <felixmarcelo1998@gmail.com>
2023-07-23 11:59:11 -0300

    ex4_3 pronto

--- ex-cap-04/js/ex4_2.js
+++ ex-cap-04/js/ex4_2.js
@@ -11,12 +11,17 @@ frm.addEventListener("submit", (e) => {
     const altura = Number(frm.inAltura.value);
     const masculino = frm.inMasculino.checked;
 
+    /*
     let peso;// declaração da variável
     if(masculino) { //ou (masculino == true)
         peso = 22 * Math.pow(altura, 2);  //Math.pow eleva ao quadrado      
     } else {
         peso = 21 * Math.pow(altura, 2);
     }
+    */
+
+    //usando operador ternário
+    const peso = masculino ? 22 * Math.pow(altura, 2) : 21 * Math.pow(altura, 2);
 
     //apresenta resposta
     respPesoIdeal.innerText = `${nome}, seu peso ideal é ${peso.toFixed(2)}Kg.`;
