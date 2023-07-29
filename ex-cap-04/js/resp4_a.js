c046350fe4c7c34a278dc939b084f6642728e861
felixmarcelo7 <felixmarcelo1998@gmail.com>
2023-07-28 22:26:47 -0300

    resp4_a pronto

---
+++ ex-cap-04/js/resp4_a.js
@@ -0,0 +1,15 @@
+//Programa Par ou ímpar
+const frm = document.querySelector("form");
+const resp = document.querySelector("h3");
+
+frm.addEventListener("submit", (e) => {
+    const numero = Number(frm.inNumero.value);
+    
+    if(numero % 2 == 0) {
+        resp.innerText = `${numero} é Par`;
+    } else {
+        resp.innerText = `${numero} é Ímpar`;
+    };
+
+    e.preventDefault();
+});
