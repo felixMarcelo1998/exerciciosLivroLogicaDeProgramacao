adeeee3f6784a4edd23e94ac5b4b36c592dc934c
felixmarcelo <felixmarcelo1998@gmail.com>
2023-10-23 13:48:45 -0300

    ex6_7 pronto

---
+++ ex-cap-06/nodejs/ex6_7.js
@@ -0,0 +1,39 @@
+//PROGRAMA DE SAQUES
+const prompt = require("prompt-sync")();
+console.log("Diguite o valor que deseja sacar (Digite 0 para sair)");
+const saques = [];
+let saqueInvalido = 0;
+
+do {
+    const valor = Number(prompt("Valor: R$"));
+    if(valor == 0) {
+        break;
+    } else if (valor % 10 != 0){
+        saqueInvalido ++;
+        console.log("Valor inválido, o caixa contém apenas cedulas de R$10,00");
+    } else {
+        console.log("Saque realizado com sucesso!");
+        saques.push(valor);
+    };
+
+} while(true);
+
+
+if(saques.length != 0) {
+    console.log(`\nSaques Válidos:\n${"-".repeat(30)}`);
+    for(const saque of saques) {
+        console.log(`R$${saque.toFixed(2)}`);
+    };
+    console.log(`${"-".repeat(30)}`);
+
+    const totSaques = saques.reduce((acc, saque) => acc + saque, 0);
+    console.log(`Total dos Saques: R$${totSaques.toFixed(2)}`);
+    console.log(`N° de Tentativas de Saques (saques inválidos): ${saqueInvalido}`);
+} else {
+    console.log(`Não há saques válidos reguistrados\n${"-".repeat(30)}\nSaques inválidos: ${saqueInvalido}`);
+};
+
+
+
+
+
