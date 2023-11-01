101c91203d012289d2b12e6562893acc5d0167a4
felixmarcelo <felixmarcelo1998@gmail.com>
2023-11-01 13:59:18 -0300

    alteracao no ex7_10 feita - tronco da arvore - pronto

--- ex-cap-07/nodejs/ex7_10.js
+++ ex-cap-07/nodejs/ex7_10.js
@@ -7,6 +7,11 @@ let arvore = "";
 for(let i = 1; i <= numLinhas; i++) {
     const espacos = 30 + (numLinhas - i);
     arvore += `${" ".repeat(espacos) + "*".repeat(2 * i)}\n`;
+    
+};
+
+for(let i = 1; i <= 3; i++) {
+    arvore += " ".repeat(30 + numLinhas - 1) + "*".repeat(2) + "\n";
 };
 
 console.log(arvore);
